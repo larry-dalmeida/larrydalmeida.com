aac83c6279716d4062e0aa75472cb1ef7e75015e
Larry D Almeida <hello@larrydalmeida.com>
2023-09-17 14:35:49 +0200

    Update meta data

---
+++ app/constants.ts
@@ -0,0 +1,6 @@
+// HEADER
+export const LOGO_TEXT = 'Larry D Almeida'
+
+// META
+export const META_DESCRIPTION = "My digital garden where I write notes on building products for the web, engineering leadership and personal recipes."
+export const BASE_URL = "https://larrydalmeida.com"
