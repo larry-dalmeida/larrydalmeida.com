3a6d1bca543c8d7a5e98f9fe5a3adc60340cac52
Larry D Almeida <hello@larrydalmeida.com>
2024-01-17 19:19:50 +0100

    Big Bang

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { LOGO_TEXT, META_DESCRIPTION } from './constants'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: `Notes, Snippets & Prose | ${LOGO_TEXT}`,
+  description: META_DESCRIPTION,
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`antialiased min-h-screen bg-slate-950 text-slate-50 ${inter.className}`}>
+        <div className="max-w-2xl mx-auto py-10 px-4">
+            <header className='text-sm font-medium'>
+              {LOGO_TEXT}
+            </header>
+            <main>
+              {children}
+            </main>
+          </div>
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+
+const Hero = () => (
+  <section className="my-16">
+    <h1>Hey! I&apos;m Larry.</h1>
+    <p className="mt-0">Welcome to my little corner of the internet.</p>
+    <p>I&apos;m a Software Engineer living in Berlin, Germany.</p>
+    <p>
+      I love building{" "}
+      <a
+        href="https://en.zalando.de/campaigns/adaptive-fashion/"
+        target="_blank"
+      >
+        innovative
+      </a>{" "}
+      products for the Web whilst helping teams foster an inclusive, high
+      performing culture that enables them to{" "}
+      <a href="https://en.zalando.de/about-sustainability/" target="_blank">raise the bar</a> in
+      online fashion retail.
+    </p>
+    <p>
+      I enjoy listening to spectrum of{" "}
+      <a
+        href="https://open.spotify.com/playlist/31F0jQTNwQDsc5gDd71q4p?si=345d354962a54f5f"
+        target="_blank"
+      >
+        music
+      </a>
+      , watching terribly good{" "}
+      <a
+        href="https://www.imdb.com/title/tt2724064/?ref_=ext_shr_lnk"
+        target="_blank"
+      >
+        movies
+      </a>{" "}
+      and indulging my{" "}
+      <a
+        href="https://media.giphy.com/media/JJE87NYU69EnS/giphy.gif"
+        target="_blank"
+      >
+        sweet teeth
+      </a>
+      .
+    </p>
+    <p>
+      If you&apos;d like to say hi you can contact me by email at{" "}
+      <Link href="mailto:hello@larrydalmeida.com">hello@larrydalmeida.com</Link>
+      .
+    </p>
+  </section>
+);
+
+export default function Home() {
+  return (
+    <div className="prose prose-invert">
+      <Hero />
+    </div>
+  );
+}
